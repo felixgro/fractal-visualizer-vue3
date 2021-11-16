6698c03fe6bae05bee1a0eb5fd7a6afc54ed3f48
felixgro <me@felixgrohs.dev>
2021-11-16 17:00:09 +0100

    refactor comment

--- src/utils/vue.ts
+++ src/utils/vue.ts
@@ -5,10 +5,10 @@ export interface ScopedWatchOptions<T> {
     ignore?: (keyof T)[];
 }
 
-// generate array for watched config keys and exclude ignored keys when specified in opts.ignore
 export const watchScoped = <T extends { [key: string]: any }>(data: T, watchEffect: WatchCallback, opts?: ScopedWatchOptions<T>) => {
     const watchedKeys: (() => keyof T)[] = [];
 
+    // generate array for watched config keys and exclude ignored keys when specified in opts.ignore
     for (const key in data) {
         if (opts?.ignore && opts.ignore.indexOf(key) >= 0) continue;
         watchedKeys.push(() => data[key]);
