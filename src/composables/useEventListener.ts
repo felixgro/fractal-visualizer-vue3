18d6c2bb88009fe06278c05cc0c1460ff4733d4a
felixgro <me@felixgrohs.dev>
2021-11-16 12:43:30 +0100

    add(composable): useEventListener

---
+++ src/composables/useEventListener.ts
@@ -0,0 +1,30 @@
+import { Ref, ref, onMounted, onUnmounted } from 'vue';
+
+export type EventSubject = Ref | Window;
+
+export type EventHandler = (e: Event) => void;
+
+export type EventOptions = {
+    immediate?: boolean;
+}
+
+const useEventListener = (subject: EventSubject, event: string, handler: EventHandler, opts?: EventOptions) => {
+    onMounted(() => {
+        if (subject === window) {
+            subject.addEventListener(event, handler);
+        } else if ((subject as Ref).value) {
+            (subject as Ref).value.addEventListener(event, handler);
+        }
+        if (opts?.immediate) handler.call({}, new Event('Initial Event Call'));
+    });
+
+    onUnmounted(() => {
+        if (subject === window) {
+            subject.removeEventListener(event, handler);
+        } else if ((subject as Ref).value) {
+            (subject as Ref).value.removeEventListener(event, handler);
+        }
+    });
+};
+
+export default useEventListener;
