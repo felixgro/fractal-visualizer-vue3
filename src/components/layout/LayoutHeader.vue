6239aa580dd1872e6a81808df9dedeb1098165d9
felixgro <me@felixgrohs.dev>
2021-11-16 16:57:04 +0100

    more refactoring

--- src/components/layout/LayoutHeader.vue
+++ src/components/layout/LayoutHeader.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
 import MainLogo from '@/components/main/MainLogo.vue';
+import { inject } from 'vue';
+
+const state = inject('state') as any;
 </script>
 
 <template>
 	<header>
 		<MainLogo />
+		{{ state.state.width }}
 	</header>
 </template>
