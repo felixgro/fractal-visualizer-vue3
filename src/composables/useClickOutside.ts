d50d46a9153d1e468609bd5a798ab4e8c2d21cb8
felixgro <me@felixgrohs.dev>
2021-11-26 18:57:22 +0100

    add(composable): useClickOutside

---
+++ src/composables/useClickOutside.ts
@@ -0,0 +1,28 @@
+import { Ref } from 'vue';
+import useEventListener from '@/composables/useEventListener';
+
+type RefElement = Ref<HTMLElement | undefined>;
+
+interface ClickOutsideConfig {
+    ignore?: RefElement[];
+}
+
+const useClickOutside = (target: RefElement, handler: (e: Event) => void, config?: ClickOutsideConfig) => {
+    const clickHandler = (e: Event) => {
+        if (!target.value) return;
+        const clickTarget = e.target as Element;
+
+        if (config?.ignore) {
+            const isIgnored = config.ignore.some(ref => ref.value === clickTarget);
+            if (isIgnored) return;
+        }
+
+        if (!target.value.contains(clickTarget)) {
+            handler(e);
+        }
+    };
+
+    useEventListener(window, 'click', clickHandler);
+}
+
+export default useClickOutside;
